1cf3c1a7a9319d4a1850bb3ceca34ef6ca9c8168
Ronny Wouters <ozzy.wouters@telenet.be>
2025-12-02 23:43:40 +0100

    getCollcetion.js

---
+++ src/services/getCollection.js
@@ -0,0 +1,11 @@
+import {firestoreDatabase} from "./connectDB";
+
+/*Collectie onbekend returns []
+Collectie leeg returns []*/
+export async function getCollection(collection){
+    if (!firestoreDatabase) return [];
+    const result=await firestoreDatabase.collection(collection).get();
+    if (result.empty) return [];
+
+    return result.docs.map(doc=>({...doc.data()}));
+}
